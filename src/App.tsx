421d0db57b125835eb5916e8d7f335ec6c0641df
ElCarambola <yakovchikdenis@gmail.com>
2025-02-03 22:00:57 +0300

    update: the functionality of the menu button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import './App.css'
 
 import Header from './components/header/Header.tsx'
 
+import { useState } from 'react'
+
 function App() {
+  const [tab, setTab] = useState<string>('About')
 
   return (
     <>
-      <Header></Header>
+      <Header active={tab} onChange={setTab}></Header>
+
+      {tab === 'About' && <p>Abouth</p>}
+      {tab === 'Selected Works' && <p>Selected Works</p>}
+      {tab === 'Side Projects' && <p>Side Projects</p>}
     </>
   )
 }
--- src/components/UI/button/menuButton/MenuButton.tsx
+++ src/components/UI/button/menuButton/MenuButton.tsx
@@ -1,13 +1,14 @@
 import './MenuButton.css'
 
 interface Props {
-    path: string;
     children: React.ReactNode;
+    active: boolean;
+    path?: string;
+    onClick?: any;
 };
 
-
-function MenuButton({ path, children }: Props) {
-    return (<li className='menu-button'>
+function MenuButton({ children, active, path = '#', onClick }: Props) {
+    return (<li className={active ? 'menu-button active' : 'menu-button'} onClick={() => onClick()}>
         <a href={path} className='menu-button-text'>{children}</a>
     </li>)
 }
