1e12ab293336c97be7730c65dec13d1803d8629c
ElCarambola <yakovchikdenis@gmail.com>
2025-02-13 20:58:47 +0300

    update: styles in UI components

--- src/components/UI/token/Token.tsx
+++ src/components/UI/token/Token.tsx
@@ -6,13 +6,25 @@ interface Props {
     specific: string;
 }
 
-export default function Token({value, global, alias, specific} : Props) {
-    return(
+export default function Token({ value, global, alias, specific }: Props) {
+    return (
         <ul className={classes.list}>
-            <li className={classes.item}> <span style={{background: `${value}`}} className={classes.color}></span> <p>{value}</p></li>
-            <li className={classes.item}> <span style={{background: `${value}`}} className={classes.color}></span> <p>{global}</p></li>
-            <li className={classes.item}> <span style={{background: `${value}`}} className={classes.color}></span> <p>{alias}</p></li>
-            <li className={classes.item}> <span style={{background: `${value}`}} className={classes.color}></span> <p>{specific}</p></li>
+            <li className={classes.item}>
+                <span style={{ background: `${value}` }} className={classes.color}></span>
+                <p className={classes.text}>{value}</p>
+            </li>
+            <li className={classes.item}>
+                <span style={{ background: `${value}` }} className={classes.color}></span>
+                <p  className={classes.text}>{global}</p>
+            </li>
+            <li className={classes.item}>
+                <span style={{ background: `${value}` }} className={classes.color}></span>
+                <p  className={classes.text}>{alias}</p>
+            </li>
+            <li className={classes.item}>
+                <span style={{ background: `${value}` }} className={classes.color}></span>
+                <p  className={classes.last}>{specific}</p>
+            </li>
 
             <li className={classes.name}>Value</li>
             <li className={classes.name}>Global Token</li>
