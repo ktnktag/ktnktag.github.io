d7a4960ac0ab7577fad27a1c2c774849955fbf10
ElCarambola <yakovchikdenis@gmail.com>
2025-05-09 23:22:37 +0300

    fix: path header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 
 import ArrowUp from './components/UI/button/ArrowUp/ArrowUp.tsx'
-// import Header from './components/header/Header.tsx'
 import Header from './components/Header/Header.tsx'
 import Footer from './components/Footer/Footer.tsx'
 
@@ -10,23 +9,23 @@ import { useRef, useEffect } from 'react'
 
 export default function App() {
   const lastScroll = useRef<number>(0);
-  const header = useRef<Element>();
+  // const header = useRef<Element>();
   const arrowUp = useRef<Element>();
 
   const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-  const containHide = () => header.current?.classList.contains('hide');
+  // const containHide = () => header.current?.classList.contains('hide');
   const containHideArrow = () => arrowUp.current?.classList.contains('show');
 
   useEffect(() => {
-    header.current = document.querySelector('.header') as Element;
+    // header.current = document.querySelector('.header') as Element;
     arrowUp.current = document.querySelector('.arrow-up') as Element;
 
     window.addEventListener('scroll', () => {
       if (scrollPosition() > lastScroll.current) {
-        // header show
-        if (!containHide() && header.current?.children.length as number > 1) {
-          header.current?.classList.add('hide');
-        }
+        // // header show
+        // if (!containHide() && header.current?.children.length as number > 1) {
+        //   header.current?.classList.add('hide');
+        // }
 
         // button show
         if (containHideArrow()) {
@@ -34,9 +33,9 @@ export default function App() {
         }
       }
       else if (scrollPosition() < lastScroll.current) {
-        if (containHide()) {
-          header.current?.classList.remove('hide');
-        }
+        // if (containHide()) {
+        //   header.current?.classList.remove('hide');
+        // }
 
         if (!containHideArrow()) {
           arrowUp.current?.classList.add('show');
