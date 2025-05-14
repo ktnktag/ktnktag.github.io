6039da57f7fde281e8964d505b1e46e3b95fe5c2
ElCarambola <yakovchikdenis@gmail.com>
2025-05-14 16:45:44 +0300

    update: footer style

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,9 @@ import Logo from '../../assets/svg/Logo.svg'
 import { Link } from 'react-router-dom'
 import LinkButton from '../UI/button/LinkButton/LinkButton'
 
-import { default as In} from '../../assets/svg/In.svg?react'
-import { default as Medium} from '../../assets/svg/Medium.svg?react'
-import { default as Vec} from '../../assets/svg/Vec.svg?react'
+import { default as In } from '../../assets/svg/In.svg?react'
+import { default as Medium } from '../../assets/svg/Medium.svg?react'
+import { default as Vec } from '../../assets/svg/Vec.svg?react'
 import { default as Be } from '../../assets/svg/Be.svg?react'
 import { default as Figma } from '../../assets/svg/Figma.svg?react'
 
@@ -14,14 +14,18 @@ export default function Footer() {
     return <footer className={classes.footer}>
         <div className='alignment'>
             <div className={classes.menu}>
-                <ul className={classes.link}>
-                    <li><Link to='/'><img src={Logo} alt="Logo" className={classes.logo}/></Link></li>
-                    <li><Link to='/projects'>Projects</Link></li>
-                    <li><Link to='/side-activity'>Side activity</Link></li>
-                    <li><Link to='/about'>About</Link></li>
-                    <li><Link to='/resume'>Resume</Link></li>
-                    <li><Link to='/'>Contact</Link></li>
-                </ul>
+                <div className={classes.page}>
+                    <Link to='/'><img src={Logo} alt="Logo" className={classes.logo} /></Link>
+
+                    <ul className={classes.link}>
+                        <li><Link to='/projects'>Projects</Link></li>
+                        <li><Link to='/side-activity'>Side activity</Link></li>
+                        <li><Link to='/about'>About</Link></li>
+                        <li><Link to='/resume'>Resume</Link></li>
+                        <li><Link to='/'>Contact</Link></li>
+                    </ul>
+                </div>
+
                 <ul className={classes.messeger}>
                     <li><LinkButton small={true} path='https://www.linkedin.com/in/kto-nekto/'><In /></LinkButton></li>
                     <li><LinkButton small={true} path='https://medium.com/@kto.nekto'><Medium /></LinkButton></li>
@@ -31,7 +35,7 @@ export default function Footer() {
                 </ul>
             </div>
 
-            <hr className={classes.separator}/>
+            <hr className={classes.separator} />
 
             <p className={classes.text}>© 2025 Valeria Yakovchik. All right reserved.</p>
         </div>
