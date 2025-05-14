28d0a66df99e63676f0f564c3d46999499b347b7
ElCarambola <yakovchikdenis@gmail.com>
2025-05-14 22:57:37 +0300

    added: sign in page

---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,39 @@
+import classes from './SignIn.module.css'
+
+import Eye from '../../assets/svg/Eye.svg'
+import EyeOff from '../../assets/svg/Eye-off.svg'
+import {default as Lock} from '../../assets/svg/lock.svg?react'
+import {default as Return} from '../../assets/svg/return.svg?react'
+
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+export default function SignIn() {
+    const [password, setPassword] = useState('');
+    const [isShow, setIsShow] = useState('password');
+
+    return <section className={classes.container}>
+        <div>
+            <h1>Private content</h1>
+            <p>Enter passcode to continue. <br /> Contact me if you need access to the project</p>
+        </div>
+
+        <form className={classes.form}>
+            <div className={classes.inputBox}>
+                <input
+                    type={isShow}
+                    placeholder='Enter passcode here'
+                    className={classes.input}
+                    value={password}
+                    onChange={(evt) => setPassword(evt.target.value)}/>
+                <img src={isShow == "password" ? EyeOff : Eye} alt="eye" className={classes.icon} onClick={() => { setIsShow(isShow === "password" ? "text" : "password") }} />
+            </div>
+            <label className={classes.button}>
+                <Lock />
+                <input type="submit" value={"Unlock"}  />
+            </label>
+        </form>
+
+        <Link to='/' className={classes.return}> <Return />Return to Projects</Link>
+    </section>
+}
--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,22 +1,30 @@
 import { createHashRouter, createRoutesFromElements, Route } from "react-router-dom";
 
 import App from "../App";
+import Shell from "../Shell.tsx";
+
 import Projects from "../pages/Projects/Projects.tsx";
 import About from "../pages/About/About.tsx";
 import Home from "../pages/Home/Home.tsx";
 import Resume from "../pages/Resume/Resume.tsx";
 import SideActivity from "../pages/SideActivity/SideActivity.tsx";
 
+import SignIn from "../pages/SignIn/SignIn.tsx";
+
 import Error404 from "./Error404.tsx";
 
 const router = createHashRouter(
     createRoutesFromElements(
-        <Route path='/' element={<App />} errorElement={<Error404 />}>
-            <Route index={true} path="/" element={<Home />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="about" element={<About />} />
-            <Route path="resume" element={<Resume />}/>
-            <Route path="side-activity" element={<SideActivity />}/>
+        <Route path='/' element={<Shell />} errorElement={<Error404 />}>
+            <Route path="/" element={<App />}>
+                <Route index={true} path="/" element={<Home />} />
+                <Route path="projects" element={<Projects />} />
+                <Route path="about" element={<About />} />
+                <Route path="resume" element={<Resume />}/>
+                <Route path="side-activity" element={<SideActivity />}/>
+            </Route>
+
+            <Route path="/auth/sign-in" element={<SignIn />}/>
         </Route>
     )
 )
